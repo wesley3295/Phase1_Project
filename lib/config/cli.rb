fee1776cde6331a5d3b2e51c45c5ed26e69b348c
Wesley <wspencer3295@yahoo.com>
2020-12-03 16:03:28 -0700

    update colums

--- lib/config/cli.rb
+++ lib/config/cli.rb
@@ -65,11 +65,34 @@ stats_logo
     end
   
     def list_champions
-        Champions.all.each.with_index(1) do |champion, index|
-            puts "#{index}. #{champion.name}"
-        end
+      col1 = 0
+      col2 = 38
+      col3 = 76
+      col4 = 114
+      standardize
+      while col1 < 38
+      puts "  #{col1+1}.#{Champions.all[col1].name} \t\t #{col2+1}.#{Champions.all[col2].name} \t\t #{col3+1}.#{Champions.all[col3].name} \t\t #{col4+1}.#{Champions.all[col4].name}"
+      
+      col1 += 1
+      col2 += 1
+      col3 += 1
+      col4 += 1
+      end
+      # puts "#{index}. #{champion.name}"
+      #  Champions.with_index(1) do |champion, index|
         # champ_detail_menu
     end
+
+    def standardize
+      max_length = 25
+
+      Champions.all.each do |champ|
+          while champ.name.length < max_length
+              champ.name += " "
+          end
+      end
+  end
+
 
     def invalid_option
         puts "Invalid input. Please try again!"
@@ -399,6 +422,8 @@ puts "
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 end
+
+
 
 end
 
